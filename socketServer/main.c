b845ae9c67bd7d506944da45ecd94f843929e92e
luke <luke.lkj@taobao.com>
2012-01-25 15:16:17 +0800

    使用kqueue改写Server

--- socketServer/main.c
+++ socketServer/main.c
@@ -10,6 +10,9 @@
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
+#include <sys/event.h>
+#include <sys/types.h>
+#include <sys/time.h>
 #include <arpa/inet.h>
 #include <string.h>
 #include <unistd.h>
@@ -23,6 +26,8 @@
 #define QUIT_CMD ".quit"
 
 int client_fds[CONCURRENT_MAX];
+
+struct kevent events[10];//CONCURRENT_MAX + 2
 
 int main (int argc, const char * argv[])
 {
@@ -58,121 +63,111 @@ int main (int argc, const char * argv[])
         return 1;
     }
     
-    //fd_set
-    fd_set server_fd_set;
-    int max_fd = -1;
-    struct timeval tv;
-    tv.tv_sec = 20;
-    tv.tv_usec = 0;
+    struct timespec timeout = {10,0};
     
+    //kqueue
+    
+    int kq = kqueue();
+    if (kq == -1) {
+        perror("创建kqueue出错!\n");
+        exit(1);
+    }
+    
+    struct kevent event_change;
+    EV_SET(&event_change, STDIN_FILENO, EVFILT_READ, EV_ADD, 0, 0, NULL);
+    kevent(kq, &event_change, 1, NULL, 0, NULL);
+    EV_SET(&event_change, server_sock_fd, EVFILT_READ, EV_ADD, 0, 0, NULL);
+    kevent(kq, &event_change, 1, NULL, 0, NULL);
+
     while (1) {
-        FD_ZERO(&server_fd_set);
-        
-        //标准输入
-        FD_SET(STDIN_FILENO, &server_fd_set);
-        if (max_fd < STDIN_FILENO) {
-            max_fd = STDIN_FILENO;
-        }
-        
-        //服务器端socket
-        FD_SET(server_sock_fd, &server_fd_set);
-        if (max_fd < server_sock_fd) {
-            max_fd = server_sock_fd;
-        }
-        
-        //客户端连接
-        for (int i = 0; i < CONCURRENT_MAX; i++) {
-            if (client_fds[i]!=0) {
-                FD_SET(client_fds[i], &server_fd_set);
-                
-                if (max_fd < client_fds[i]) {
-                    max_fd = client_fds[i];
-                }
-            }
-        }
-        
-        int ret = select(max_fd+1, &server_fd_set, NULL, NULL, &tv);
-        
+        int ret = kevent(kq, NULL, 0, events, 10, &timeout);
         if (ret < 0) {
-            perror("select 出错\n");
+            printf("kevent 出错!\n");
             continue;
         }else if(ret == 0){
-            printf("select 超时\n");
+            printf("kenvent 超时!\n");
             continue;
         }else{
-            //ret为未状态发生变化的文件描述符的个数
-            if (FD_ISSET(STDIN_FILENO, &server_fd_set)) {
-                //标准输入
-                bzero(input_msg, BUFFER_SIZE);
-                fgets(input_msg, BUFFER_SIZE, stdin);
+            //ret > 0 返回事件放在events中
+            for (int i = 0; i < ret; i++) {
+                struct kevent current_event = events[i];
                 
-                //输入 ".quit" 则退出服务器
-                if (strcmp(input_msg, QUIT_CMD) == 0) {
-                    exit(0);
-                }
-                
-                for (int i=0; i<CONCURRENT_MAX; i++) {
-                    if (client_fds[i]!=0) {
-                        send(client_fds[i], input_msg, BUFFER_SIZE, 0);
+                if (current_event.ident == STDIN_FILENO) {
+                    //标准输入
+                    bzero(input_msg, BUFFER_SIZE);
+                    fgets(input_msg, BUFFER_SIZE, stdin);
+                    
+                    //输入 ".quit" 则退出服务器
+                    if (strcmp(input_msg, QUIT_CMD) == 0) {
+                        exit(0);
                     }
-                }
-            }
-            
-            if (FD_ISSET(server_sock_fd, &server_fd_set)) {
-                //有新的连接请求
-                struct sockaddr_in client_address;
-                socklen_t address_len;
-                int client_socket_fd = accept(server_sock_fd, (struct sockaddr *)&client_address, &address_len);
-                
-                if (client_socket_fd > 0) {
-                    int index = -1;
-                    for (int i = 0; i < CONCURRENT_MAX; i++) {
-                        if (client_fds[i] == 0) {
-                            index = i;
-                            client_fds[i] = client_socket_fd;
-                            break;
+                    
+                    for (int i=0; i<CONCURRENT_MAX; i++) {
+                        if (client_fds[i]!=0) {
+                            send(client_fds[i], input_msg, BUFFER_SIZE, 0);
                         }
                     }
-                    if (index >= 0) {
-                        printf("新客户端(%d)加入成功 %s:%d \n",index,inet_ntoa(client_address.sin_addr),ntohs(client_address.sin_port));
-                    }else{
-                        bzero(input_msg, BUFFER_SIZE);
-                        strcpy(input_msg, "服务器加入的客户端数达到最大值,无法加入!\n");
-                        send(client_socket_fd, input_msg, BUFFER_SIZE, 0);
-                        printf("客户端连接数达到最大值，新客户端加入失败 %s:%d \n",inet_ntoa(client_address.sin_addr),ntohs(client_address.sin_port));
-                    }
-
-                }
-            }
-            
-            for (int i = 0; i <CONCURRENT_MAX; i++) {
-                if (client_fds[i]!=0) {
-                    if (FD_ISSET(client_fds[i], &server_fd_set)) {
-                        //处理某个客户端过来的消息
-                        bzero(recv_msg, BUFFER_SIZE);
-                        long byte_num = recv(client_fds[i],recv_msg,BUFFER_SIZE,0);
-                        if (byte_num > 0) {
-                            if (byte_num > BUFFER_SIZE) {
-                                byte_num = BUFFER_SIZE;
+                }else if(current_event.ident == server_sock_fd){
+                    //有新的连接请求
+                    struct sockaddr_in client_address;
+                    socklen_t address_len;
+                    int client_socket_fd = accept(server_sock_fd, (struct sockaddr *)&client_address, &address_len);
+                    
+                    if (client_socket_fd > 0) {
+                        int index = -1;
+                        for (int i = 0; i < CONCURRENT_MAX; i++) {
+                            if (client_fds[i] == 0) {
+                                index = i;
+                                client_fds[i] = client_socket_fd;
+                                break;
                             }
-                            recv_msg[byte_num] = '\0';
-                            printf("客户端(%d):%s\n",i,recv_msg);
-                        }else if(byte_num < 0){
-                            printf("从客户端(%d)接受消息出错.\n",i);
-                        }else{
-                            FD_CLR(client_fds[i], &server_fd_set);
-                            client_fds[i] = 0;
-                            printf("客户端(%d)退出了\n",i);
                         }
-
+                        if (index >= 0) {
+                            EV_SET(&event_change, client_socket_fd, EVFILT_READ, EV_ADD, 0, 0, NULL);
+                            kevent(kq, &event_change, 1, NULL, 0, NULL);
+                            
+                            printf("新客户端(fd = %d)加入成功 %s:%d \n",client_socket_fd,inet_ntoa(client_address.sin_addr),ntohs(client_address.sin_port));
+                        }else{
+                            bzero(input_msg, BUFFER_SIZE);
+                            strcpy(input_msg, "服务器加入的客户端数达到最大值,无法加入!\n");
+                            send(client_socket_fd, input_msg, BUFFER_SIZE, 0);
+                            printf("客户端连接数达到最大值，新客户端加入失败 %s:%d \n",inet_ntoa(client_address.sin_addr),ntohs(client_address.sin_port));
+                        }
+                        
+                    }
+                    
+                }else{
+                    //处理某个客户端过来的消息
+                    bzero(recv_msg, BUFFER_SIZE);
+                    long byte_num = recv((int)current_event.ident,recv_msg,BUFFER_SIZE,0);
+                    if (byte_num > 0) {
+                        if (byte_num > BUFFER_SIZE) {
+                            byte_num = BUFFER_SIZE;
+                        }
+                        recv_msg[byte_num] = '\0';
+                        printf("客户端(fd = %d):%s\n",(int)current_event.ident,recv_msg);
+                    }else if(byte_num < 0){
+                        printf("从客户端(fd = %d)接受消息出错.\n",(int)current_event.ident);
+                    }else{
+                        EV_SET(&event_change, current_event.ident, EVFILT_READ, EV_DELETE, 0, 0, NULL);
+                        kevent(kq, &event_change, 1, NULL, 0, NULL);
+                        close((int)current_event.ident);
+                        
+                        for (int i = 0; i < CONCURRENT_MAX; i++) {
+                            if (client_fds[i] == (int)current_event.ident) {
+                                client_fds[i] = 0;
+                                break;
+                            }
+                        }
+                        
+                        printf("客户端(fd = %d)退出了\n",(int)current_event.ident);
                     }
                 }
+                
             }
-            
         }
-        
-        
     }
+    
     return 0;
 }
 
